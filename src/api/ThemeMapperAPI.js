ef4f59e790aa930b76d43c3e5332e8e096a26741
Nicolas Thebaud <nicolas.thebaud@wiztivi.com>
2021-01-07 10:14:55 +0100

    implement axios

--- src/api/ThemeMapperAPI.js
+++ src/api/ThemeMapperAPI.js
@@ -1,25 +1,11 @@
-import io from 'socket.io-client';
-import Events from '../constants/ThemeMap';
+import axios from 'axios';
 
-
-let socket;
-const socketAddr = 'localhost';
-const socketPort = 1234;
 
 export default function useThemeMap() {
-    if (!socket) {
-        socket = io(`ws://${socketAddr}:${socketPort}`, {});
-    }
 
     async function getThemes() {
         console.log('[TMAP] Fetching themes...');
-        socket.emit(Events.FETCH_THEMES);
-
-        const response = new Promise((resolve) => {
-            socket.on(Events.FETCH_THEMES, resolve);
-        });
-        const themes = await response;
-        return themes;
+        return (await axios.get('http://localhost:1234/getThemes')).data;
     }
 
     return [getThemes];
--- src/api/TokenAPI.js
+++ src/api/TokenAPI.js
@@ -1,40 +1,19 @@
-import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import axios from 'axios';
 
-
-let socket;
-const socketAddr = 'localhost';
-const socketPort = 9666;
-const socketUrl = `ws://${socketAddr}:${socketPort}?wtvChromeExt=true`;
 
 export default function useTokenApi() {
-    if (!socket) {
-        socket = new W3CWebSocket(socketUrl);
-        socket.onopen = () => console.log(`[WS] Websocket opened! [${socketUrl}]`);
-        socket.onError = error => console.error(`[WS] WebSocket error: ${JSON.stringify(error)}`);
-    }
-
-    function close() {
-        socket.close();
-    }
-
     /**
      * send single token to Websocket (UI) and to the database
      *
      * @param {Object} token - data format: {key: <String>, value: <String>} with <key> being the token's name
      */
     function setSingleToken(token = {}) {
-        if (!socket) { throw new Error('[WS] no socket opened'); }
-
-        if (Object.keys(token).length) {
-            console.log('[API] send data:', token);
-            const data = {
-                class: 'Theme',
-                method: 'setDesignTokenValue',
-                args: [token.key, token.value, ''],
-                guid: 'whatisthat',
-            };
-            socket.send(JSON.stringify(data));
-        }
+        axios.post('http://localhost:4444/setDisplayTokenValue', {
+            class: 'Theme',
+            method: 'setDesignTokenValue',
+            args: [token.key, token.value, ''],
+            guid: 'whatisthat',
+        });
     }
 
     /**
@@ -44,12 +23,7 @@ export default function useTokenApi() {
      *      {key: <String>, value: <String>} with <key> being the token's name
      */
     function setTokens(tokens = []) {
-        if (!socket) { throw new Error('[WS] no socket opened'); }
-
-        if (tokens.length) {
-            socket.send(JSON.stringify(tokens));
-        }
     }
 
-    return [setSingleToken, setTokens, close];
+    return [setSingleToken, setTokens];
 };
